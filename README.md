ec3720923d3df0131a0a268791f2805a134747dd
eleoelo <obsidiancat534@gmail.com>
2023-10-16 01:30:56 +0300

    Added the basics

---
+++ README.md
@@ -0,0 +1,4 @@
+# **Welcome to Linea!**
+
+This is a C++ library built on top of SDL to work with simple 3D Graphics.
+The work-flow is really linear (hence the name) so it allows you to create your cute indie 3D games!
---
+++ math/basics.hpp
@@ -0,0 +1,15 @@
+namespace math
+{   
+    //Returns the factorial of a number
+    //If a number is negative, it returns -1
+    long long fctr(int n) {
+        if (n < 1) return -1;
+        long long result = 1;
+        for (int i = 0; i < n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    double sin(double n);
+} 
