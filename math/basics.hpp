73dfba3327b73e725f4646ba6f0571fcab84bac2
eleoelo <obsidiancat534@gmail.com>
2023-10-16 09:04:13 +0300

    Added new functions to basics.hpp

--- math/basics.hpp
+++ math/basics.hpp
@@ -2,13 +2,31 @@ namespace math
 {   
     //Returns the factorial of a number
     //If a number is negative, it returns -1
-    long long fctr(int n) {
-        if (n < 1) return -1;
+    long long fctr(int number) 
+    {
+        if (number < 1) return -1;
         long long result = 1;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < number; i++) {
             result *= i;
         }
         return result;
     }
 
+    //Gets the number in a certain power
+    long long toPower(int number, int power)
+    {
+        long long result = 1;
+        for (int i = 0; i < power; i++) 
+        {
+            result *= number;
+        }
+        return number;
+    }
+
+    //Returns the number squared
+    long long sqr(int number) 
+    {
+        return toPower(number, 2);
+    } 
+    
 } 
