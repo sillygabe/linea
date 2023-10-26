669c0139151bb94cfc225ac7a4b48d9da8f778fe
eleoelo <obsidiancat534@gmail.com>
2023-10-26 09:23:23 +0300

    Removed .DS_store

--- Builder/builder.cpp
+++ Builder/builder.cpp
@@ -1,59 +1,27 @@
-#include <stdlib.h>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
-
-//SOMEONE PLEASE FIX THIS HORRIBLE CODE!!!!
-
-const char *programText =
-"#include <Linea>\n"
-"#include \"projsettings.hpp\"\n"
-"\n\n"
-"int main()\n"
-"{\n\treturn 0;\n}\n";
+using namespace std;
 
 int main(int argc, char **argv)
 {
     if (argc < 2) 
     {
-        std::cout << "Not enough arguements.\n";
+        cout << "No commands" << endl;
         return 0;
     }
-
-    std::string command = argv[1];
+    string command = argv[2];
 
     if (command == "init")
     {
-        std::string author, projectName;
+        string author, project;
+        cout << "Enter dev's nickname: ";
+        cin >> author;
+        cout << "Enter project's name";
+        cin >> project;
 
-        std::cout << "Initializing the project...\n";
-        std::cout << "Enter your name: ";
-        std::cin >> author;
-        std::cout << "Enter project name: ";
-        std::cin >> projectName;
-
-        command = "mkdir "; command += projectName;
-        system(command.c_str());
-
-        command = "cd "; 
-        command += projectName;
-        command += " && touch project.yml";
-        system(command.c_str());
-
-        command = "cd "; 
-        command += projectName + " && touch projsettings.hpp";
-        system(command.c_str());
-        command = "cd "; 
-        command += projectName + "&& touch main.cpp";
-
-        system(command.c_str());
-
-        command = projectName + "/main.cpp";
-        
-        std::fstream file; file.open(command);
-        file << programText;
-
-        std::cout << "Project initialized.\n";
+        fstream yml;
+        yml.open()
     }
 
     return 0;
